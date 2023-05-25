46e141928de6a59ecb5142ea85e2795942d7dbee
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-05-25 17:12:57 +0300

    add, separation of files according to FCD methodology, and configuration of absolute imports

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import './styles/index.scss';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
+	return (
+		<div className={classNames('app', {}, [theme])}>
+			<button onClick={toggleTheme}>Toggle</button>
+			<Link to={'/'}>Main</Link>
+			<Link to={'/about'}>About</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/about'} element={<AboutPage />} />
+					<Route path={'/'} element={<MainPage />} />
+				</Routes>
+			</Suspense>
+		</div>
+	);
+};
+
+export default App;
