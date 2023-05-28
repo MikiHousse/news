12e48ce299a4e9b712fc64aa6a0733edfe2c5c91
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-05-28 10:31:25 +0300

    add BundleAnalyzerPlugin and error boundary

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,20 +2,22 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HtmlWebpackPlugin({
-      template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ];
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css',
+		}),
+		new webpack.DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev),
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new BundleAnalyzerPlugin({ analyzerMode: (process.env.STATS as 'server') || 'disabled' }),
+	];
 }
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -8,18 +8,16 @@ export enum ThemeButton {
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	className?: string;
-	theme: ThemeButton;
+	theme?: ThemeButton;
 }
 export const Button: FC<ButtonProps> = (props) => {
-  const {
-    className, children, theme, ...otherProps
-  } = props;
+	const { className, children, theme, ...otherProps } = props;
 
-  return (
-    <button {...otherProps} className={classNames(cls.Button, {}, [className, cls[theme]])}>
-      {children}
-    </button>
-  );
+	return (
+		<button {...otherProps} className={classNames(cls.Button, {}, [className, cls[theme]])}>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
