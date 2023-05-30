78c200f2bfd91d54b2cbc013109a92c92fa05111
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-05-30 07:34:44 +0300

    add fix bag storybook and loki

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta, Meta, StoryObj } from '@storybook/react'
 import { Theme } from 'app/providers/ThemeProvider';
 import { Navbar } from './Navbar';
 import ThemeDecorator from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import StyleDecorator from 'shared/config/storybook/StyleDecorator/StyleDecorator';
+import RouterDecorator from 'shared/config/storybook/RouterDecorator/RouterDecorator';
 
 const meta: Meta<typeof Navbar> = {
 	title: 'widget/Navbar',
@@ -16,7 +16,8 @@ type Story = StoryObj<typeof Navbar>;
 export const Light: Story = {
 	args: {},
 };
-
+Light.decorators = [ThemeDecorator(Theme.DARK), RouterDecorator];
 export const Dark: Story = {
 	args: {},
 };
+Dark.decorators = [ThemeDecorator(Theme.LIGHT), RouterDecorator];
